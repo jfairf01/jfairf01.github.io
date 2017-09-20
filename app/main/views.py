dec19674779128551a3eb92ef51c14ea071215f5
John Fairfield-Sonn <johntfairfieldsonn@gmail.com>
2017-09-20 15:17:57 -0400

    Reverted away from Python app

--- app/main/views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from ..models import EditableHTML
-
-from . import main
-
-
-@main.route('/')
-def index():
-    return render_template('main/index.html')
---
+++ node-example/README.md
@@ -0,0 +1,3 @@
+#This whole folder was made using the code from Ming Chow in Comp 20.
+#John Fairfield-Sonn
+#Tuesday November 1st
